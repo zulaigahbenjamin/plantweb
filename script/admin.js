daec1c27a05172df0daa58036cf475f4719985b5
zulaigahbenjamin <130163047+zulaigahbenjamin@users.noreply.github.com>
2023-06-15 16:15:13 +0200

    final

--- script/admin.js
+++ script/admin.js
@@ -1,19 +1,173 @@
 
 
+// // Sample data to simulate the backend
+// let plants = [
+//   { id: 1, 
+//     name: 'Plant A',
+//      image: 'https://i.postimg.cc/Xv648Sww/wood-succulant-stand.jpg',
+//      price: 2000, 
+//     },
+
+//   { 
+//     id: 2, 
+//     name: 'Plant B',
+//      image: 'https://i.postimg.cc/Xv648Sww/wood-succulant-stand.jpg',
+//      price:2000,
+//     },
+// ];
+
+// // render the plant list
+// function renderPlantList() {
+//   const plantList = document.getElementById('plantList');
+//   plantList.innerHTML = '';
+
+//   plants.forEach(plant => {
+//     const listItem = document.createElement('li');
+//     listItem.innerHTML = `
+//       <img src="${plant.image}" alt="${plant.name}" width="100">
+//       <span>${plant.name}</span>
+//       <div>${plant.name}</div>
+//       <div>${plant.price}</div>
+//       <button class="editButton" data-id="${plant.id}">Edit</button>
+//       <button class="deleteButton" data-id="${plant.id}">Delete</button>
+//     `;
+//     plantList.appendChild(listItem);
+//   });
+// }
+
+// //  handle submission
+// function addPlant(event) {
+//   event.preventDefault();
+
+//   const plantNameInput = document.getElementById('plantName');
+//   const plantImageInput = document.getElementById('plantImage');
+//   const plantPriceInput = document.getElementById('plantPrice');
+
+//   const plantName = plantNameInput.value;
+//   const plantImage = plantImageInput.value;
+//   const plantPrice = plantPriceInput.value;
+
+//   if (plantName && plantImage && plantPrice) {
+//     const newPlantId = plants.length + 1;
+
+//     //  new plant object
+//     const newPlant = {
+//       id: newPlantId,
+//       name: plantName,
+//       image: plantImage,
+//       price: parseInt(plantPrice),
+//     };
+
+//     // Add new plant to the list
+//     plants.push(newPlant);
+
+//     // Render updated 
+//     renderPlantList();
+
+//     // Reset 
+//     plantNameInput.value = '';
+//     plantImageInput.value = '';
+//     plantPriceInput.value = '';
+//   }
+// }
+
+// // delete button clicks
+// function deletePlant(event) {
+//   const plantId = parseInt(event.target.dataset.id);
+//   plants = plants.filter(plant => plant.id !== plantId);
+//   renderPlantList();
+// }
+
+// //  edit button 
+// function editPlant(event) {
+//   const plantId = parseInt(event.target.dataset.id);
+//   const plant = plants.find(plant => plant.id === plantId);
+//   if (plant) {
+//     // Implement your edit functionality here
+//     console.log(`Edit plant with ID: ${plant.id}`);
+//   }
+// }
+
+// // event listeners 
+// const addPlantForm = document.getElementById('addPlantForm');
+// addPlantForm.addEventListener('submit', addPlant);
+
+// const plantList = document.getElementById('plantList');
+// plantList.addEventListener('click', event => {
+//   if (event.target.classList.contains('deleteButton')) {
+//     deletePlant(event);
+//   } else if (event.target.classList.contains('editButton')) {
+//     editPlant(event);
+//   }
+// });
+
+// // rendering plant list
+// renderPlantList();
+
+
+
 // Sample data to simulate the backend
 let plants = [
-  { id: 1, 
-    name: 'Plant A',
-     image: 'https://i.postimg.cc/Xv648Sww/wood-succulant-stand.jpg',
-     price: 2000, 
-    },
-
-  { 
-    id: 2, 
-    name: 'Plant B',
-     image: 'https://i.postimg.cc/Xv648Sww/wood-succulant-stand.jpg',
-     price:2000,
-    },
+  {
+    id: 1,
+    name: 'Aloe',
+    image: 'https://i.postimg.cc/Xv648Sww/wood-succulant-stand.jpg',
+    price: "R2000",
+  },
+  {
+    id: 2,
+    name: 'Aloe',
+    image: 'https://i.postimg.cc/Xv648Sww/wood-succulant-stand.jpg',
+    price:  'R2000',
+  },
+  {
+    id: 1,
+    name: 'Aloe',
+    image: 'https://i.postimg.cc/Xv648Sww/wood-succulant-stand.jpg',
+    price: "R2000",
+  },
+  {
+    id: 2,
+    name: 'Aloe',
+    image: 'https://i.postimg.cc/Xv648Sww/wood-succulant-stand.jpg',
+    price:  'R2000',
+  },
+  {
+    id: 1,
+    name: 'Aloe',
+    image: 'https://i.postimg.cc/Xv648Sww/wood-succulant-stand.jpg',
+    price: "R2000",
+  },
+  {
+    id: 2,
+    name: 'Aloe',
+    image: 'https://i.postimg.cc/Xv648Sww/wood-succulant-stand.jpg',
+    price:  'R2000',
+  },
+  {
+    id: 1,
+    name: 'Aloe',
+    image: 'https://i.postimg.cc/Xv648Sww/wood-succulant-stand.jpg',
+    price: "R2000",
+  },
+  {
+    id: 2,
+    name: 'Aloe',
+    image: 'https://i.postimg.cc/Xv648Sww/wood-succulant-stand.jpg',
+    price:  'R2000',
+  },
+  {
+    id: 1,
+    name: 'Aloe',
+    image: 'https://i.postimg.cc/Xv648Sww/wood-succulant-stand.jpg',
+    price: "R2000",
+  },
+  {
+    id: 2,
+    name: 'Aloe',
+    image: 'https://i.postimg.cc/Xv648Sww/wood-succulant-stand.jpg',
+    price:  'R2000',
+  },
 ];
 
 // render the plant list
@@ -25,7 +179,6 @@ function renderPlantList() {
     const listItem = document.createElement('li');
     listItem.innerHTML = `
       <img src="${plant.image}" alt="${plant.name}" width="100">
-      <span>${plant.name}</span>
       <div>${plant.name}</div>
       <div>${plant.price}</div>
       <button class="editButton" data-id="${plant.id}">Edit</button>
@@ -35,56 +188,60 @@ function renderPlantList() {
   });
 }
 
-//  handle submission
+// handle submission
 function addPlant(event) {
   event.preventDefault();
 
   const plantNameInput = document.getElementById('plantName');
   const plantImageInput = document.getElementById('plantImage');
+  const plantPriceInput = document.getElementById('plantPrice');
 
   const plantName = plantNameInput.value;
   const plantImage = plantImageInput.value;
+  const plantPrice = plantPriceInput.value;
 
-  if (plantName && plantImage) {
+  if (plantName && plantImage && plantPrice) {
     const newPlantId = plants.length + 1;
 
-    //  new plant object
+    // new plant object
     const newPlant = {
       id: newPlantId,
       name: plantName,
       image: plantImage,
+      price: parseInt(plantPrice),
     };
 
-    // Add new plant to the list
+    // new plant to list
     plants.push(newPlant);
 
-    // Render updated 
+    // Render updated list
     renderPlantList();
 
     // Reset 
     plantNameInput.value = '';
     plantImageInput.value = '';
+    plantPriceInput.value = '';
   }
 }
 
-// delete button clicks
+// delete button
 function deletePlant(event) {
   const plantId = parseInt(event.target.dataset.id);
   plants = plants.filter(plant => plant.id !== plantId);
   renderPlantList();
 }
 
-//  edit button 
+// edit button
 function editPlant(event) {
   const plantId = parseInt(event.target.dataset.id);
   const plant = plants.find(plant => plant.id === plantId);
   if (plant) {
-    // Implement your edit functionality here
+    // edit
     console.log(`Edit plant with ID: ${plant.id}`);
   }
 }
 
-// event listeners 
+// listeners
 const addPlantForm = document.getElementById('addPlantForm');
 addPlantForm.addEventListener('submit', addPlant);
 
@@ -99,9 +256,6 @@ plantList.addEventListener('click', event => {
 
 // rendering plant list
 renderPlantList();
-
-
-
 
 
 
